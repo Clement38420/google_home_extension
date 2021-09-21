41acea01a637578d02ffea3cb9fbd93a3491ec98
Clement Charbonnel <c.charbo38@gmail.com>
2021-09-21 20:05:40 +0000

    Added .env port for server

--- app.js
+++ app.js
@@ -28,13 +28,8 @@ app.get("/", (req, res) => {
 	res.status(200).send("Hello world")
 })
 
-const httpServer = http.createServer(app)
 const httpsServer = https.createServer(credentials, app)
 
-httpServer.listen(80, () => {
-	console.log("HTTP Server running on port 80")
-})
-
-httpsServer.listen(443, () => {
-	console.log("HTTPS Server running on port 443")
+httpsServer.listen(process.env.PORT, () => {
+	console.log("HTTPS Server running")
 })
